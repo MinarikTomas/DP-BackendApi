6fb11e6065ae69164df47e443eb3570fbd0556cb
MinarikTomas <tminarik20@gmail.com>
2023-10-28 00:04:59 +0200

    Initial commit

---
+++ src/main/java/sk/stuba/fei/uim/dp/attendanceapi/AttendanceApiApplication.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.dp.attendanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/stuba/fei/uim/dp/attendanceapi/AttendanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.dp.attendanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
